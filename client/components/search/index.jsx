8f08bd5dd13fb910abeb86baff9deb29240a508c
marekhrabe <marekhrabe@me.com>
2016-06-04 23:37:49 +0200

    Initial implementation of keyboard controls for SiteSelector

--- client/components/search/index.jsx
+++ client/components/search/index.jsx
@@ -160,7 +160,8 @@ const Search = React.createClass( {
 			: this.props.onSearch;
 
 		if ( this.props.autoFocus ) {
-			this.focus();
+			// this hack makes autoFocus work correctly in Dropdown
+			setTimeout( () => this.focus(), 0 );
 		}
 	},
 
