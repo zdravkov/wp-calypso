940542adc7d62fc0473a97ec7d3ba9b3ba160161
Omar Jackman <github@omarjackman.com>
2016-09-17 11:08:25 -0700

    Add shortcodes reducer with a real implementation

--- client/state/shortcodes/reducer.js
+++ client/state/shortcodes/reducer.js
@@ -1,17 +1,40 @@
 /**
- * External dependencies
+ * Internal dependencies
  */
-import { combineReducers } from 'redux';
+import { createReducer } from 'state/utils';
+import { LOAD_STATUS } from './constants';
+import {
+	SHORTCODE_FETCH,
+	SHORTCODE_RECEIVE
+} from 'state/action-types';
 
-export function status( state = {} ) {
-	return state;
-}
-
-export function shortcodes( state = {} ) {
-	return state;
-}
-
-export default combineReducers( {
-	status,
-	shortcodes
+const siteShortcodeStatus = createReducer( null, {
+	[ SHORTCODE_FETCH ]: ( state ) => {
+		return { ...state, ...{ status: LOAD_STATUS.LOADING } };
+	},
+	[ SHORTCODE_RECEIVE ]: ( state, { error } ) => {
+		return { ...state, ...{ status: error ? LOAD_STATUS.ERROR : LOAD_STATUS.LOADED } };
+	},
 } );
+
+const siteShortcodes = ( state = {}, action ) => {
+	switch ( action.type ) {
+		case SHORTCODE_FETCH:
+		case SHORTCODE_RECEIVE:
+			const { shortcode } = action;
+			return { ...state, ...{ [ shortcode ]: siteShortcodeStatus( state[ shortcode ] || {}, action ) } };
+	}
+
+	return state;
+};
+
+export default ( state = {}, action ) => {
+	switch ( action.type ) {
+		case SHORTCODE_FETCH:
+		case SHORTCODE_RECEIVE:
+			const { siteId } = action;
+			return { ...state, ...{ [ siteId ]: siteShortcodes( state[ siteId ] || {}, action ) } };
+	}
+
+	return state;
+};
