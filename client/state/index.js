6bf00daca7e917f3ba61ad58e8155655c6f78ba5
Omar Jackman <github@omarjackman.com>
2016-09-16 17:02:14 -0700

    Add empty reducer for the new shortcodes redux store

--- client/state/index.js
+++ client/state/index.js
@@ -34,6 +34,7 @@ import purchases from './purchases/reducer';
 import reader from './reader/reducer';
 import receipts from './receipts/reducer';
 import sharing from './sharing/reducer';
+import shortcodes from './shortcodes/reducer';
 import signup from './signup/reducer';
 import sites from './sites/reducer';
 import siteSettings from './site-settings/reducer';
@@ -75,6 +76,7 @@ export const reducer = combineReducers( {
 	reader,
 	receipts,
 	sharing,
+	shortcodes,
 	signup,
 	sites,
 	siteSettings,
