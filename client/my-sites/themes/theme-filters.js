0d816e06b41db3e2a80b8a418e1e6ae8adfa9756
budzanowski <bartosz.budzanowski@gmail.com>
2016-09-29 17:39:03 -0700

    Push currently edited text to suggestions
    Does not work - cursor position calculation is wrong.

--- client/my-sites/themes/theme-filters.js
+++ client/my-sites/themes/theme-filters.js
@@ -172,6 +172,13 @@ const taxonomies = {
 };
 /* eslint-enable */
 
+/**
+ * @return {Object} [taxonomies][terms]
+ */
+export function getTaxonomies( ) {
+  return taxonomies;
+}
+
 let termTable;
 
 /**
