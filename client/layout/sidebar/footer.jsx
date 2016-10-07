f4a4891118fa3f7794ecbd53b6b6f196c48385e5
Joen Asmussen <joen@automattic.com>
2016-10-07 12:22:12 +0200

    Sidebar Footer: Start work on new, more flexible, layout
    This PR starts work on #8461. It:
    
    - Adjusts the layout to fit the mockups
    - Adds the "Add new site" button to sit there permanently, so we don't have to have a separate switchin layout for the site selector.
    
    Still lots to do.
    
    - Tracks for "Add new site" wasn't ported.
    - We should probably update the sidebar component entirely to use the new translate function.
    - Multisite switcher needs to work with this new design.
    - Unread dot design for chat button.
    
    There's probably lots more, some of it I'll need help with (translate, multisite switcher, tracks?), so would appreciate help. CC: @mtias @rickybanister

--- client/layout/sidebar/footer.jsx
+++ client/layout/sidebar/footer.jsx
@@ -18,13 +18,16 @@ import config from 'config';
 const SidebarFooter = ( { translate, children, onOpenChat } ) => (
 	<div className="sidebar__footer">
 		{ children }
-		<Button compact borderless href="/help">
-			<Gridicon icon="help-outline" /> { translate( 'Help' ) }
+		<Button borderless href={ config( 'signup_url' ) + '?ref=calypso-selector' }>
+			<Gridicon icon="add-outline" /> { translate( 'Add New Site' ) }
+		</Button>
+		<Button className="sidebar__footer__help" borderless href="/help" title={ translate( 'Help' ) }>
+			<Gridicon icon="help-outline" />
 		</Button>
 		{ config.isEnabled( 'happychat' )
 			? (
-			<Button className="sidebar__footer__support-chat" compact borderless onClick={ onOpenChat }>
-				<svg className="gridicon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18"><path d="M16 3h-6c-1.1 0-2 .9-2 2v1h1c1.654 0 3 1.346 3 3v1h2v3l3.415-3.415c.375-.375.585-.882.585-1.412V5c0-1.1-.9-2-2-2z" /><path d="M3 7h6c1.1 0 2 .9 2 2v3c0 1.1-.9 2-2 2H5v3l-3.415-3.415C1.21 13.21 1 12.703 1 12.173V9c0-1.1.9-2 2-2z" /></svg> { translate( 'Support Chat' ) }
+			<Button className="sidebar__footer__support-chat" borderless onClick={ onOpenChat } title={ translate( 'Support Chat' ) }>
+				<svg className="gridicon" width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g><path d="M20 4h-8c-1.1 0-2 .9-2 2v2h2c1.7 0 3 1.3 3 3v2h2v3.5l3.3-2.3c1.1-.8 1.7-2 1.7-3.3V6c0-1.1-.9-2-2-2z"/><path d="M14 11v5c0 1.1-.9 2-2 2H7v3.5l-3.3-2.3c-1.1-.8-1.7-2-1.7-3.3V11c0-1.1.9-2 2-2h8c1.1 0 2 .9 2 2z"/></g></svg>
 			</Button>
 			) : null
 		}
